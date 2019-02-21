b4d4b34b9b1bd869c59ecf9e163a9606fbadef3e
Damien Picard <damien.picard@axeiya.com>
2019-02-21 16:21:33 +0100

    Update setup.py
    Try to fix travis build

--- setup.py
+++ setup.py
@@ -87,6 +87,7 @@ setup_args = dict(
     include_package_data = True,
     install_requires = [
         'ipywidgets>=7.0.0',
+        'opencv-python>=3.4.5.20',
     ],
     extras_require = {
         'test': [
